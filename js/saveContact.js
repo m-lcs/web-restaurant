61394d0f1f7cd3609680324ef9d7d1e7edde8d81
timotedieu <117929719+timotedieu@users.noreply.github.com>
2025-01-24 14:46:29 +0100

    dernier modif

--- js/saveContact.js
+++ js/saveContact.js
@@ -1,18 +1,15 @@
-// Écouteur pour le champ "name"
 document.getElementById("name").addEventListener("input", function () {
     const name = document.getElementById("name").value;
     localStorage.setItem("name", name);
     console.log("Nom sauvegardé :", name);
 });
 
-// Écouteur pour le champ "email"
 document.getElementById("email").addEventListener("input", function () {
     const email = document.getElementById("email").value;
     localStorage.setItem("email", email);
     console.log("Email sauvegardé :", email);
 });
 
-// Chargement des données à l'ouverture de la page
 window.onload = function () {
     const savedName = localStorage.getItem("name");
     const savedEmail = localStorage.getItem("email");
